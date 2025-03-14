3cc43596e298d79fe636a918b793b9b25ee14e72
Ujjwal <epicujjwal@protonmail.com>
2025-03-15 03:04:03 +0530

    broadcast: this better pass the test everytime

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"encoding/json"
 	"log"
-	"sync"
+	"time"
 
 	"slices"
 
@@ -12,82 +12,59 @@ import (
 
 func main() {
 	n := maelstrom.NewNode()
-	var values []float64 = make([]float64, 0)
-	var leftover []map[string]any
+	var values []float64
+
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 		var body map[string]any
-		err := json.Unmarshal(msg.Body, &body)
-		if err != nil {
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
-		var val float64 = body["message"].(float64)
-		var f uint8 = 0
-		if slices.Contains(values, val) {
-			f = 1
-		}
-		if f == 0 {
+
+		val := body["message"].(float64)
+		if !slices.Contains(values, val) {
 			values = append(values, val)
 			nodes := n.NodeIDs()
-			var wg1 sync.WaitGroup
-			wg1.Add(len(nodes))
 			for _, neighbour := range nodes {
-				go func(neighbour string) {
-					n.RPC(neighbour, body, func(msg maelstrom.Message) error {
-						var response map[string]any
-						err := json.Unmarshal(msg.Body, &response)
-						if response["type"] == "error" {
-							leftover = append(leftover, body)
-						}
-						wg1.Done()
-						return err
-					})
-				}(neighbour)
+				if neighbour != n.ID() && neighbour != msg.Src {
+					go broadcast(10*time.Millisecond, n, neighbour, body)
+				}
 			}
-			var wg2 sync.WaitGroup
-			wg2.Add(len(leftover))
-
-			for _, msg := range leftover {
-				go func(msg map[string]any) {
-					for _, neighbour := range nodes {
-						n.Send(neighbour, msg)
-					}
-					wg2.Done()
-				}(msg)
-			}
-			wg1.Wait()
-			wg2.Wait()
 		}
-		body["type"] = "broadcast_ok"
-		delete(body, "message")
-		return n.Reply(msg, body)
+
+		reply := map[string]any{"type": "broadcast_ok"}
+		return n.Reply(msg, reply)
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
-		var body map[string]any
-		err := json.Unmarshal(msg.Body, &body)
-		if err != nil {
-			return err
+		reply := map[string]any{
+			"type":     "read_ok",
+			"messages": values,
 		}
-
-		body["type"] = "read_ok"
-		body["messages"] = values
-
-		return n.Reply(msg, body)
+		return n.Reply(msg, reply)
 	})
 
 	n.Handle("topology", func(msg maelstrom.Message) error {
-		var body map[string]any
-		err := json.Unmarshal(msg.Body, &body)
-		if err != nil {
-			return err
-		}
-		body["type"] = "topology_ok"
-		delete(body, "topology")
-		return n.Reply(msg, body)
+		reply := map[string]any{"type": "topology_ok"}
+		return n.Reply(msg, reply)
 	})
 
-	err := n.Run()
-	if err != nil {
+	n.Handle("broadcast_ok", func(msg maelstrom.Message) error {
+		return nil
+	})
+
+	if err := n.Run(); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func broadcast(backoff time.Duration, node *maelstrom.Node, dest string, body map[string]any) {
+	replied := false
+	node.RPC(dest, body, func(msg maelstrom.Message) error {
+		replied = true
+		return nil
+	})
+	time.Sleep(backoff)
+	if !replied {
+		broadcast(backoff*2, node, dest, body)
 	}
 }
